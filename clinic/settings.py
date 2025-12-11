1c4a4b6aa031ab9ce87892f23d444719f8dc88ef
darkognk <golovicdarko@gmail.com>
2025-12-11 10:36:49 +0100

    ..

--- clinic/settings.py
+++ clinic/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 STATIC_URL = '/static/'
 
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / "static"]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
